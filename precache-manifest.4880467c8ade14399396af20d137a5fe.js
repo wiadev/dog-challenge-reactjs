46e627676ac154269ad39b1a274cfbccc15340f0
wiadev <wiadev@users.noreply.github.com>
2020-02-27 00:37:57 -0800

    Updates

---
+++ precache-manifest.4880467c8ade14399396af20d137a5fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa5f0d0226ad72f6e654dd5de7bc1ff",
+    "url": "/dog-challenge-reactjs/index.html"
+  },
+  {
+    "revision": "9604f70dc08daa8bd55d",
+    "url": "/dog-challenge-reactjs/static/css/main.1abb1b65.chunk.css"
+  },
+  {
+    "revision": "84643adb8aa6fe94e7df",
+    "url": "/dog-challenge-reactjs/static/js/2.32a1aa13.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/dog-challenge-reactjs/static/js/2.32a1aa13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9604f70dc08daa8bd55d",
+    "url": "/dog-challenge-reactjs/static/js/main.adc091fb.chunk.js"
+  },
+  {
+    "revision": "59474e600d7fc4962cad",
+    "url": "/dog-challenge-reactjs/static/js/runtime-main.37f8aad3.js"
+  }
+]);
